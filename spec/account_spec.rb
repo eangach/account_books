f88320398157af1ff8e19604dfb2627435ce4739
Thomas Behrndt <thomas.behrndt@me.com>
2015-08-02 08:28:14 +0930

    Add initial Account.count check to tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,17 +3,23 @@ require_relative 'spec_helper'
 module AccountBooks
   describe Account do
     it 'has two accounts' do
+      Account.count.must_equal 0
+
       Account.create(name: 'Test Account One', type: :asset)
       Account.create(name: 'Test Account Two', type: :asset)
       Account.count.must_equal 2
     end
 
     it 'has one account' do
+      Account.count.must_equal 0
+
       Account.create(name: 'Test Account Three', type: :asset)
       Account.count.must_equal 1
     end
 
     it 'cannot have duplicate account names' do
+      Account.count.must_equal 0
+
       Account.create(name: 'Test Account One', type: :asset)
       Account.count.must_equal 1
       Account.create(name: 'Test Account One', type: :asset)
